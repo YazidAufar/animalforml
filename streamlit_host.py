52e0093f2940f4a02459fcb12c8aa580d61d4c6b
YazidAufar <60860675+YazidAufar@users.noreply.github.com>
2022-01-12 12:19:36 +0800

    Update streamlit_host.py

--- streamlit_host.py
+++ streamlit_host.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as 
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
 
 
